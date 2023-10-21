6d8f6e75aa5ec806e8a57ab6b61b39560a77bcc8
GeraTaker <20030097@itcelaya.edu.mx>
2023-10-20 23:31:05 -0600

    Configuracion de tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div class="bg-gray-100 h-screen flex items-center justify-center">
+    <div class="border border-blue-600 bg-white p-8 rounded shadow-lg text-center">
+        <h1 class="text-3xl font-semibold mb-4">¡Hola Mundo!</h1>
+        <p class="text-gray-600">Este es un ejemplo de "Hola Mundo" usando Tailwind CSS.</p>
+    </div>
+</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
