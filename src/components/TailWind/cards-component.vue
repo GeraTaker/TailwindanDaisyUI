2129e83c8f73d7616c975f8a75073da295a9c1d2
GeraTaker <20030097@itcelaya.edu.mx>
2023-10-23 11:55:41 -0600

    Paginas agregadas Vue tilawind and daisyUI

---
+++ src/components/TailWind/cards-component.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Games</h1>
+  <div class="grid sm:grid-cols-1 xl:grid-cols-4 gap-4">
+    <div v-for="(game, index) in games" :key="index" class="bg-gradient-to-br  from-green-500 to-blue-500">
+      <h2>{{ game.title }}</h2>
+      <img class="animate-bounce mt-9" :src="game.thumbnail" />
+      <a :href="game.game_url">Jugar ahora</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    games: null,
+  },
+};
+</script>
